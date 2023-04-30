cd9f2f37df464af0060f5aa9d766339c51f41da5
Melanie Hendricks <melanie.r.hendricks@gmail.com>
2023-04-30 15:27:27 -0700

    new build

---
+++ build/precache-manifest.398527de6aead0db995730583f227740.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "91dbd6e2827a03c5123d",
+    "url": "/static/css/main.397a8ddf.chunk.css"
+  },
+  {
+    "revision": "91dbd6e2827a03c5123d",
+    "url": "/static/js/main.91dbd6e2.chunk.js"
+  },
+  {
+    "revision": "bbec7dc8d58ec3f5adc9",
+    "url": "/static/js/1.bbec7dc8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "44317eb20baa9918d29716d3149ec521",
+    "url": "/index.html"
+  }
+];
